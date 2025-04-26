8e36a9d84012c1726bb43aed71347064251b63bd
Vladislav <vladislavlotockij1@gmail.com>
2025-04-26 11:08:46 +0300

    ref: added comments for the code

--- app/Http/Controllers/Api/Auth/LoginController.php
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -5,23 +5,29 @@ namespace App\Http\Controllers\Api\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\Auth\LoginRequest;
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
-    public function __invoke(LoginRequest $request)
+    /**
+     * Handle an incoming authentication request.
+     *
+     * @param LoginRequest $request The login request containing email and password
+     * @return JsonResponse
+     */
+    public function __invoke(LoginRequest $request): JsonResponse
     {
         $data = $request->validated();
 
         $existingUser = User::where('email', $data['email'])->first();
 
-        if (!$existingUser || Hash::check($data['password'], $existingUser->password))
+        if (!$existingUser || !Hash::check($data['password'], $existingUser->password))
             return response()->json(['message' => 'Invalid credentials.'], 401);
 
         $token = $existingUser->createToken('auth_token')->plainTextToken;
 
         return response()->json([
-            'message' => 'Login successful.',
             'token' => $token,
         ], 200);
     }
